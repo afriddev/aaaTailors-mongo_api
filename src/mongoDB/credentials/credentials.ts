af9456f9e14ebe00335b1289bef788078fcac9f7
AFRID SHAIK <afridayan01@gmail.com>
2023-10-19 23:38:22 +0530

    changed password

--- src/mongoDB/credentials/credentials.ts
+++ src/mongoDB/credentials/credentials.ts
@@ -5,6 +5,6 @@
  * mongo db credentials
  * 
  */
-const password:string = "21765A0217"
+const password:string = "he he he he"
 const mongo_url:string = `mongodb+srv://afriddev:${password}@ameerunisatailors.o7xouiz.mongodb.net/users?retryWrites=true&w=majority&appName=AtlasApp`
 export default  mongo_url

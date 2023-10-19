017b2523faea199192dac6fe4bcb9f2c758fd869
AFRID SHAIK <afridayan01@gmail.com>
2023-10-19 23:38:50 +0530

    changed password

--- dist/mongoDB/credentials/credentials.js
+++ dist/mongoDB/credentials/credentials.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * mongo db credentials
  *
  */
-const password = "21765A0217";
+const password = "he he he he ";
 const mongo_url = `mongodb+srv://afriddev:${password}@ameerunisatailors.o7xouiz.mongodb.net/users?retryWrites=true&w=majority&appName=AtlasApp`;
 exports.default = mongo_url;
